f096e4ec046aefc9f514f4c2e4bcf6716a579043
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-26 20:16:49 +0530

    Updating module

--- app/src/main/java/com/example/pokedex/di/AppModule.kt
+++ app/src/main/java/com/example/pokedex/di/AppModule.kt
@@ -1,10 +1,34 @@
 package com.example.pokedex.di
 
+import com.example.pokedex.data.remote.PokemonApi
+import com.example.pokedex.repository.PokemonRepository
+import com.example.pokedex.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesPokemonApi(): PokemonApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .build()
+            .create(PokemonApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesPokemonRepository(
+        api: PokemonApi
+    ) = PokemonRepository(api)
+
 }
