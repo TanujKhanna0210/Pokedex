d9ea8821e20b53eeadccfad85543f1666524d216
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-26 17:48:19 +0530

    Setting up navigation

---
+++ app/src/main/java/com/example/pokedex/navigation/AppNavigation.kt
@@ -0,0 +1,39 @@
+package com.example.pokedex.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.POKEMON_LIST_SCREEN) {
+        composable(Routes.POKEMON_LIST_SCREEN) {
+            // Pokemon list screen composable here
+        }
+        composable("${Routes.POKEMON_DETAIL_SCREEN}/{dominantColor}/{pokemonName}",
+            arguments = listOf(
+                navArgument("dominantColor") {
+                    type = NavType.IntType
+                },
+                navArgument("pokemonName") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val dominantColor = remember {
+                val color = it.arguments?.getInt("dominantColor")
+                color?.let { Color(it) } ?: Color.White
+            }
+            val pokemonName = remember {
+                it.arguments?.getString("pokemonName")
+            }
+            // Pokemon detail screen composable here
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pokedex/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.example.pokedex.navigation
+
+object Routes {
+
+    const val POKEMON_LIST_SCREEN = "POKEMON_LIST"
+    const val POKEMON_DETAIL_SCREEN = "POKEMON_DETAIL"
+
+}
