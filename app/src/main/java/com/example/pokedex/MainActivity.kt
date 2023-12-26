0bdf0d654e5de0b8d8d6e7fc8da8ed9a61341785
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-26 17:59:00 +0530

    Setting up dagger-hilt

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.pokedex.navigation.AppNavigation
 import com.example.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
